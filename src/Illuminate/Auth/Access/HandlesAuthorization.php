344c2687a1193985ec358bbf3e619cd3daad0fd0
OneCodeMonkey <1460018362@qq.com>
2020-02-18 21:29:45 +0800

    feat: Illuminate\Auth\Access\HandlesAuthorization

---
+++ src/Illuminate/Auth/Access/HandlesAuthorization.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Illuminate\Auth\Access;
+
+trait HandlesAuthorization
+{
+    /**
+     * Create a new access response.
+     *
+     * @param string|null $message
+     * @param mixed $code
+     * @return \Illuminate\Auth\Access\Response
+     */
+    protected function allow($message = null, $code = null)
+    {
+        return Response::allow($message, $code);
+    }
+
+    /**
+     * @param string|null $message
+     * @param mixed $code
+     * @return \Illuminate\Auth\Access\Response
+     */
+    protected function deny($message = null, $code = null)
+    {
+        return Response::deny($message, $code);
+    }
+}
