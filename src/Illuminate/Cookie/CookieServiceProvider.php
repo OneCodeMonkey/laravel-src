6c642f6d619238449fa0aa2a2638c1afc8b684d6
OneCodeMonkey <1460018362@qq.com>
2020-02-26 15:37:53 +0800

    feat: Illuminate\Cookie\CookieServiceProvider

---
+++ src/Illuminate/Cookie/CookieServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Illuminate\Cookie;
+
+use GuzzleHttp\Cookie\CookieJar;
+use Illuminate\Support\ServiceProvider;
+
+class CookieServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('cookie', function ($app) {
+            $config = $app->make('config')->get('session');
+
+            return (new CookieJar)->setDefaultPathAndDomain(
+                $config['path'],
+                $config['domain'],
+                $config['secure'],
+                $config['same_site'] ?? null
+            );
+        });
+    }
+}
